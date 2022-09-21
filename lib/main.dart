e7fd3792e148916d039bb441ef7dbec4be51f0b0
mitchelgitau <mitchelgitau2@gmail.com>
2022-09-21 23:04:44 +0300

    lesson 1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'location_detail.dart';
+import 'models/location.dart';
+import 'mock/mock_location.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LocationDetail()
+  ));
+}
